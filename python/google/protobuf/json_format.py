7043b71c3f0f9e9a8499975809ef4456527bceec
Seal <info@sealsecurity.io>
2026-08-20 13:35:37 +0000

    CVE-2026-0994

--- python/google/protobuf/json_format.py
+++ python/google/protobuf/json_format.py
@@ -471,6 +471,10 @@ class _Parser(object):
     Raises:
       ParseError: In case of convert problems.
     """
+    # Increment recursion depth at message entry. The max_recursion_depth limit
+    # is exclusive: a depth value equal to max_recursion_depth will trigger an
+    # error. For example, with max_recursion_depth=5, nesting up to depth 4 is
+    # allowed, but attempting depth 5 raises ParseError.
     self.recursion_depth += 1
     if self.recursion_depth > self.max_recursion_depth:
       raise ParseError('Message too deep. Max recursion depth is {0}'.format(
@@ -644,9 +648,11 @@ class _Parser(object):
       self._ConvertWrapperMessage(value['value'], sub_message,
                                   '{0}.value'.format(path))
     elif full_name in _WKTJSONMETHODS:
-      methodcaller(_WKTJSONMETHODS[full_name][1], value['value'], sub_message,
-                   '{0}.value'.format(path))(
-                       self)
+      # For well-known types (including nested Any), use ConvertMessage
+      # to ensure recursion depth is properly tracked
+      self.ConvertMessage(
+          value['value'], sub_message, '{0}.value'.format(path)
+      )
     else:
       del value['@type']
       self._ConvertFieldValuePair(value, sub_message, path)
@@ -667,9 +673,9 @@ class _Parser(object):
   def _ConvertValueMessage(self, value, message, path):
     """Convert a JSON representation into Value message."""
     if isinstance(value, dict):
-      self._ConvertStructMessage(value, message.struct_value, path)
+      self.ConvertMessage(value, message.struct_value, path)
     elif isinstance(value, list):
-      self._ConvertListValueMessage(value, message.list_value, path)
+      self.ConvertMessage(value, message.list_value, path)
     elif value is None:
       message.null_value = 0
     elif isinstance(value, bool):
@@ -689,8 +695,8 @@ class _Parser(object):
           value, path))
     message.ClearField('values')
     for index, item in enumerate(value):
-      self._ConvertValueMessage(item, message.values.add(),
-                                '{0}[{1}]'.format(path, index))
+      self.ConvertMessage(item, message.values.add(),
+                          '{0}[{1}]'.format(path, index))
 
   def _ConvertStructMessage(self, value, message, path):
     """Convert a JSON representation into Struct message."""
@@ -701,8 +707,8 @@ class _Parser(object):
     # there are no values.
     message.Clear()
     for key in value:
-      self._ConvertValueMessage(value[key], message.fields[key],
-                                '{0}.{1}'.format(path, key))
+      self.ConvertMessage(value[key], message.fields[key],
+                          '{0}.{1}'.format(path, key))
     return
 
   def _ConvertWrapperMessage(self, value, message, path):
